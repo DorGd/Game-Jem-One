2c4d1fc4d2c8640ac8d18f73739c1af7857ba532
shani shapp <shanishapp@gmail.com>
2020-12-15 10:57:03 +0200

    update exit retry button script

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIScript : MonoBehaviour
+{
+    public void onExitClicked()
+    {
+        MyGameManager.Instance.onExitClicked();
+    }
+
+    public void onRetryClicked()
+    {
+        MyGameManager.Instance.onRetryClicked();
+    }
+}
