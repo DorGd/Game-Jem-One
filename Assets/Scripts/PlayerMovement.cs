599366ea8e93cc612ce1453f365d4cd69d927a18
DorGi <dor.gid00@gmail.com>
2020-12-15 15:27:01 +0200

    boundry + beam

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -62,7 +62,7 @@ public class PlayerMovement : MonoBehaviour
             EventManagerScript.Instance.TriggerEvent(EventManagerScript.EVENT__CRUSHED_MINION,null);
          	sr.color = new Color(2,0,0);//set this object's red color to 200 percent
         	Vector3 newLoc = gameObject.transform.position ;
-			transform.DOJump(newLoc,2,1,1,false);
+			transform.DOJump(newLoc,1,1,1,false);
 
 			StartCoroutine(flickerPlayer());
             Destroy(other.gameObject);
@@ -72,6 +72,9 @@ public class PlayerMovement : MonoBehaviour
 			EventManagerScript.Instance.TriggerEvent(EventManagerScript.EVENT__CRUSHED_MINION,null);
 		    sr.color = new Color(2,0,0);//set this object's red color to 200 percent
         	StartCoroutine(flickerPlayer());
+            Vector3 newLoc = gameObject.transform.position ;
+            transform.DOJump(newLoc,1.5f,1,1,false);
+
 		}
 		
 
